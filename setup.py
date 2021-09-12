90677861aa4d55251d12f5f09b5e91e25acff9c1
amazinglzy <1075749478@qq.com>
2021-09-12 09:12:03 +0800

    prepare for publish

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='page_query',
-    version='0.1.2',
-    py_modules=find_packages(),
+    version='0.1.5',
+    description='collect, index and query pages from everywhere',
+    author='amazinglzy',
+    author_email='forlearn_lzy@163.com',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Click',
+        'pyyaml',
+        'requests',
+        'elasticsearch',
+        'rich'
     ],
     entry_points={
         'console_scripts': [
